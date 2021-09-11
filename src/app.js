c48a919af70ce1706a9f403767f17c5b9ddafd5f
MCGiako <giacometto.r.h2@gmail.com>
2021-09-11 02:08:49 +0000

    Code Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  for (let f = 0; f < pronoun.length; f++)
+    for (let g = 0; g < adj.length; g++)
+      for (let h = 0; h < noun.length; h++) {
+        console.log(pronoun[f] + adj[g] + noun[h] + ".com");
+      }
 };
